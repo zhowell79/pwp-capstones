c1e9d7b5ba6eac0a8760ebc496221f6f34f804af
zhowell79 <41129079+zhowell79@users.noreply.github.com>
2018-11-11 12:23:09 -0700

    Codecademy generated from the project submission page.

---
+++ Tome_Rater/TomeRater.py
@@ -0,0 +1,15 @@
+class User(object):
+    def __init__(self, name, email):
+        pass
+
+    def get_email(self):
+        pass
+
+    def change_email(self, address):
+        pass
+
+    def __repr__(self):
+        pass
+
+    def __eq__(self, other_user):
+        pass
